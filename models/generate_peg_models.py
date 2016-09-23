8a55c494fd83ee828f71818ede6f2d9064ea8e47
Alex <alexanderboasjohnson@gmail.com>
2016-09-23 14:54:57 -0700

    modified comparision of models, added graph of various model performance

--- models/generate_peg_models.py
+++ models/generate_peg_models.py
@@ -2,13 +2,21 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import BaggingRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.pipeline import FeatureUnion
 from sklearn.externals import joblib
 from matplotlib import pyplot as plt
+import seaborn as sns
 import time
 from utils import ItemSelector, PegFeatureExtractor
+from collections import OrderedDict
+
+
+
+
 
 class GeneratePegModel(object):
 
@@ -30,7 +38,7 @@ class GeneratePegModel(object):
         self.X = self.peg_data
         self.y = np.array([line.pop(5) for line in self.peg_data])
 
-    def build_pipeline(self, X, y):
+    def build_pipeline(self, X, y, mod_name):
         self.dtr = Pipeline([
                             ('featureextract', PegFeatureExtractor()),
                             ('union', FeatureUnion(
@@ -43,62 +51,93 @@ class GeneratePegModel(object):
                                                     ('vect', DictVectorizer())
                                                 ]))
                                             ])),
-                            ('dtr', DecisionTreeRegressor()),
-                            ('rfr', RandomForestRegressor(n_jobs=-1)),
+                            ('mod', self.models[mod_name])
                                 ])
         self.dtr.fit(X, y)
 
-    def train_model(self, length):
+    def train_model(self, length, mod_name):
         X = self.X[:length]
         y = self.y[:length]
         self.X_train, self.X_test, self.y_train, self.y_test = \
             train_test_split(X, y, test_size=0.33, random_state=42)
-        self.build_pipeline(self.X_train, self.y_train)
+        self.build_pipeline(self.X_train, self.y_train, mod_name)
 
     def score_model(self, length):
         train_score = self.dtr.score(self.X_train, self.y_train)
         test_score = self.dtr.score(self.X_test, self.y_test)
         self.scores.append([length, train_score, test_score])
 
-    def cross_score_model(self):
-        lengths = [10, 100, 1000, 10000, 100000] # 1000000, 2000000, 3000000]#, 10000000]
+    def cross_score_model(self, mod_name):
+        lengths = [10, 100, 1000, 10000, 100000, 1000000]
         start = time.time()
         for length in lengths:
             # import pdb; pdb.set_trace()
-            self.train_model(length)
+            self.train_model(length, mod_name)
             self.score_model(length)
             print 'for length %s, duration has been %s' % \
                                                 (length, time.time() - start)
 
-    def build_cv_graph(self):
+    def build_cv_scores(self, mod_name):
         lengths = [score[0] for score in self.scores]
         train = [score[1] for score in self.scores]
         test = [score[2] for score in self.scores]
-        with open('graphs/peg_scores.txt', 'w') as sc:
-            sc.write(str(self.scores))
+        with open('graphs/peg_scores.txt', 'a') as sc:
+            sc.write(str({mod_name : self.scores}))
         print train, test
-        plt.plot(lengths, train, color='Blue', label='Train')
-        plt.plot(lengths, test, color='Red', label='Test')
+
+    def build_cv_graph(self, fig_loc):
+        with open('graphs/peg_scores.txt', 'r') as ps:
+            data = ps.read()
+            data = data.replace('}{', '},{')
+            data = eval('[' + data + ']')
+            data = reduce(lambda x,y: dict(x.items() + y.items()),data)
+
+        color_list = ['red', 'green', 'blue']
+        color_dict = {key:color_list[i] for i, key in enumerate(sorted(data.keys()))}
+        for key, value in data.iteritems():
+            x = [point[0] for point in value]
+            y = [point[1:] for point in value]
+            plt.plot(x, y, label=key, color=color_dict[key])
         plt.xlabel('# of datapoints')
         plt.ylabel('r^2 value')
-        plt.ylim([-1.5, 1.5])
+        plt.ylim([-0.5, 1.1])
         plt.xscale('log')
-        plt.legend()
-        plt.savefig('graphs/cv_scores.png')
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = OrderedDict(zip(labels, handles))
+        legend = plt.legend(by_label.values(), by_label.keys(), loc=2, frameon=True)
+        frame = legend.get_frame()
+        frame.set_facecolor('white')
+        plt.savefig(fig_loc)
 
-    def run_full_model(self, length=None):
+    def run_full_model(self, mod_name, length=None):
         if length is None:
             length = len(self.X)
-        self.build_pipeline(self.X[:length], self.y[:length])
+        self.build_pipeline(self.X[:length], self.y[:length], mod_name)
 
     def save_model(self, output_path):
         joblib.dump(self.dtr, output_path)
 
+    def cv_pipeline_models(self):
+        self.models = {'dtr': DecisionTreeRegressor(),
+                    'br': BaggingRegressor(n_jobs=-1),
+                    'rfr': RandomForestRegressor(n_jobs=-1),
+                }
+        for mod_name in self.models.keys():
+            self.scores = []
+            self.cross_score_model(mod_name)
+            self.build_cv_scores(mod_name)
+        self.build_cv_graph('graphs/peg_cv_scores.png')
+
+
+
 
 def main(input_path, output_path):
     ghm = GeneratePegModel(input_path)
-    ghm.cross_score_model()
-    ghm.build_cv_graph()
+    ghm.cv_pipeline_models()
+    ghm.run_full_model('br', 1000000)
+
+    # ghm.cross_score_model()
+    # ghm.build_cv_graph()
     # ghm.save_model(output_path)
     # print "running full model"
     # ghm.run_full_model()
