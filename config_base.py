49cc5dbaef2220b9a30d2b16c1968cfd5fc16be4
Alex <alexanderboasjohnson@gmail.com>
2016-10-03 17:59:00 -0700

    modified config base to have pw in environ var

--- config_base.py
+++ config_base.py
@@ -1,6 +1,12 @@
+import os
+
+pw = os.environ['CC_MYSQL_PW']
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="alexjohnson84",
-    password="peggers2",
+    password=pw,
     hostname="alexjohnson84.mysql.pythonanywhere-services.com",
     databasename="alexjohnson84$cribdb",
 )
+# basedir = os.path.abspath(os.path.dirname(__file__))
+#
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
